b913e6e6c014ae72c00db25934681a207ec6cbc0
Ranffi Ramirez <ranffiramirez@gmail.com>
2023-02-24 23:36:36 -0500

    included env for neo4j to draw graph

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,47 @@
 const webpack = require('webpack')
 
-module.exports = {
-	entry: ["./client/index.js"],
-	output: {
-		path: __dirname,
-		filename: "server/public/bundle.js",
-	},
-	resolve: {
-		extensions: [".js", ".jsx"],
-	},
-	devtool: "source-map",
-	watchOptions: {
-		ignored: /node_modules/,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.jsx?$/,
-				exclude: /node_modules/,
-				loader: "babel-loader",
-				options: {
-					presets: ["@babel/preset-react"],
+module.exports = () => {
+	const keys = {
+		'process.env.MAPBOXPK': JSON.stringify(process.env.MAPBOXPK),
+		'process.env.NEO4J_URI': JSON.stringify(process.env.NEO4J_URI),
+		'process.env.NEO4J_USER': JSON.stringify(process.env.NEO4J_USER),
+		'process.env.NEO4J_PASSWORD': JSON.stringify(process.env.NEO4J_PASSWORD),
+		'process.env.NEO4J_DATABASE': JSON.stringify(process.env.NEO4J_DATABASE)
+	}
+	return {
+		entry: ["./client/index.js"],
+		output: {
+			path: __dirname,
+			filename: "server/public/bundle.js",
+		},
+		resolve: {
+			extensions: [".js", ".jsx"],
+		},
+		devtool: "source-map",
+		watchOptions: {
+			ignored: /node_modules/,
+		},
+		module: {
+			rules: [
+				{
+					test: /\.jsx?$/,
+					exclude: /node_modules/,
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-react"],
+					},
 				},
-			},
-			{
-				test: /\.css$/,
-				use: [
-					"style-loader",
-					"css-loader"
-				]
-			}
-		],
-	},
-	plugins: [
-		new webpack.DefinePlugin({
-			'process.env.MAPBOXPK': JSON.stringify(process.env.MAPBOXPK),
-		})
-	],
+				{
+					test: /\.css$/,
+					use: [
+						"style-loader",
+						"css-loader"
+					]
+				}
+			],
+		},
+		plugins: [
+			new webpack.DefinePlugin(keys),
+		]
+	}
 }
